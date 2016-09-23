758c3a48e327e46e60c670b8f62633ec528a5986
L. Bolton <liamthomasbolton@gmail.com>
2016-09-23 22:42:34 +0100

    Update R Code - Machine Learning.R

--- resources/code/R Code - Machine Learning.R
+++ resources/code/R Code - Machine Learning.R
@@ -1,11 +1,18 @@
+# Machine Learning
+
 # load data from flat files (both data and labels) - you can find these in data > open-data
 pad_data <- read.csv("pima-indians-diabetes.data", header = FALSE)
 names(pad_data) <- c("no_preg","glucose","bloodpress","skinthick","insulin","BMI","dpf","age","hasdiab")
 
+install.packages("e1071")
+library(e1071)
+
 # set the classifier column to a factor - required for naive bayes methods to work 
 pad_data$hasdiab <- as.factor(pad_data$hasdiab)
 
+install.packages("caret")
 library(caret)
+
 inTrain <- createDataPartition(pad_data$hasdiab, p = 0.7, list = F)
 training_set <- pad_data[inTrain, ]
 
@@ -14,7 +21,9 @@ inTest <- createDataPartition(excluded_set$hasdiab, p = 0.5, list = F)
 test_set <- excluded_set[inTest, ] 
 cv_set <- excluded_set[-inTest, ]
 
+install.packages("ggplot2")
 library(ggplot2)
+
 g1 <- ggplot(data = training_set, aes(x = hasdiab, y = glucose, fill = hasdiab))
 g1 + geom_boxplot()
 with(training_set, t.test(glucose[hasdiab == 1], glucose[hasdiab == 0]))
@@ -24,14 +33,21 @@ test_set$hasdiab_pred <- predict(logit_model, test_set)
 test_set$hasdiab_pred <- ifelse(test_set$hasdiab_pred > 0.5,1,0)
 confusionMatrix(test_set$hasdiab_pred, test_set$hasdiab)
 
+install.packages("rpart")
 library(rpart)
+
 ct <- rpart(hasdiab ~ ., data = training_set)
 
+install.packages("rattle")
 library(rattle)
+
+install.packages("rpart.plot")
 library(rpart.plot)
+
 fancyRpartPlot(ct)
 
-library(randomForest)
-rf <- randomForest(hasdiab ~ ., data = training_set)
 
-library(e1071)
+install.packages("randomForest")
+library(randomForest)
+
+rf <- randomForest(hasdiab ~ ., data = training_set)
